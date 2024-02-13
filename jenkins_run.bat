0e91ff90ddaa87f459f77ea2425aecdcba5025c0
Kundu <2303480@cognizant.com>
2024-02-13 10:31:09 +0530

    some chnages like methid name added

---
+++ jenkins_run.bat
@@ -0,0 +1,2 @@
+cd C:\jenkins
+java -jar jenkins.war
--- src/test/java/TestCases/TC001_HomePage.java
+++ src/test/java/TestCases/TC001_HomePage.java
@@ -15,33 +15,33 @@ public class TC001_HomePage extends BaseClass {
 	//variable of HomePage
 	public HomePage hp;
 	
-	//test1()--> validating the title only
+	//titleValidation()--> validating the title only
 	@Test(priority=1,groups= {"master","regression"})
-	void test1() throws IOException {
+	void titleValidation() throws IOException {
 		hp = new HomePage(driver); //object of homePages
 		hp.titleValidation();
 		log.info("---title is validated---");
 	}
 	
-	//test2()--> hovering on usedcars and clicking on chennai city and it is dependent on test1() 
-	@Test(priority=2, dependsOnMethods= {"test1"},groups= {"master","regression"})
-	void test2() throws IOException, InterruptedException {
+	//clickOnUsedCars()--> hovering on usedcars and clicking on chennai city and it is dependent on test1() 
+	@Test(priority=2, dependsOnMethods= {"titleValidation"},groups= {"master","regression"})
+	void clickOnUsedCars() throws IOException, InterruptedException {
 		hp = new HomePage(driver); //object of homePage
 		hp.clickingOnCityForUsedCars();
 		log.info("---clicked on chennai city---");
 	}
 	
-	//test5()--> hovering on newbikes option and clickig on upcoming bikes option
+	//clickOnUpcomingBikes()--> hovering on newbikes option and clickig on upcoming bikes option
 	@Test(priority=5,groups= {"master","regression"})
-	void Test5() throws IOException {
+	void clickOnUpcomingBikes() throws IOException {
 		hp = new HomePage(driver);
 		hp.clickingOnUpcomingBikes();
 		log.info("---clicked on upcoming bikes---");
 	}
 	
-	//test9()--> clicking on login/signup, then clickign on google to provide invalid mail and capture the error message
+	//clickOnLoginSignUp()--> clicking on login/signup, then clickign on google to provide invalid mail and capture the error message
 	@Test(priority=9,groups= {"master","sanity"})
-	void Test9() throws IOException, InterruptedException {
+	void clickOnLoginSignUp() throws IOException, InterruptedException {
 		hp = new HomePage(driver); //object of homePage
 		hp.login();
 		hp.captureErrorMessage();
